297a5a001061835b7729c5bdbcc3d80bd09a02cb
saikiran2001 <bjsaikiran@gmail.com>
2023-10-23 19:17:49 +0530

    Updater: Rebrand to Pixelstar
    Change-Id: I3c7a2df5f6ac8411fbd14cf759aa3f78f6c26866
    Signed-off-by: Mudit200408 <muditpurohit2004.pm@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 import java.util.Properties
-import org.lineageos.generatebp.GenerateBpPlugin
-import org.lineageos.generatebp.GenerateBpPluginExtension
-import org.lineageos.generatebp.models.Module
+import com.pixelstar.generatebp.GenerateBpPlugin
+import com.pixelstar.generatebp.GenerateBpPluginExtension
+import com.pixelstar.generatebp.models.Module
 
 plugins {
     id("com.android.application")
@@ -18,7 +18,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.lineageos:gradle-generatebp:+")
+        classpath("com.pixelstar:gradle-generatebp:+")
     }
 }
 
@@ -33,7 +33,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "org.lineageos.updater"
+        applicationId = "com.pixelstar.updater"
         minSdk = 30
         targetSdk = 33
         versionCode = 1
